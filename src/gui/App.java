55d4c5441fc7e2067d5e9b2c6960b940a1cae52c
davydov-denis <den.73.rus@mail.ru>
2016-06-30 00:53:05 +0400

    added the actual entry point to the application

---
+++ src/gui/App.java
@@ -0,0 +1,52 @@
+package gui;
+
+import java.io.IOException;
+
+import fxmls.FXMLFrameLoader;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class App extends Application {
+
+	protected static Scene scene;
+	protected static Stage stage;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		App.stage = stage;
+		setFrame(FXMLFrameLoader.getMainFrame(), "Собери Рюкзак в поход!");
+	}
+	
+	public static void setFrame(Parent root) throws IOException{
+		setFrame(root, "");
+	}
+	
+	public static void setFrame(String frameName, String title) throws IOException{
+		setFrame(FXMLFrameLoader.getRoot(frameName), title);
+
+	}
+	
+	public static void setFrame(String frameName) throws IOException{
+		setFrame(frameName, "");
+	}
+	
+	public static void setFrame(Parent root, String title) throws IOException{
+		scene = new Scene(root);
+		
+		if (stage.isShowing())
+			stage.close();
+		
+		stage.setScene(scene);
+		stage.setTitle(title);
+		stage.show();
+	}
+
+}
