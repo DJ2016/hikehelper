fde5dc12862104452a7623d537faa90c15e8832d
bytezeroone <bytezeroone@gmail.com>
2016-06-30 20:28:37 +0400

    Revert "refactored #isChecked method"
    This reverts commit 09cb37914cb7552d5fae913424eecb30decdbd45.

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -23,22 +23,22 @@ public class Controller{
 	
 	@FXML private TextField searchField;
 	@FXML private TableView<Product> tableView;
-	@FXML private CheckBox male;
-	@FXML private CheckBox female;
+	@FXML private CheckBox checkBox1;
+	@FXML private CheckBox checkBox2;
 	
 	@FXML
 	public void isChecked(){
-		if (male.isSelected()){
-			female.setDisable(true);
+		if (checkBox1.isSelected()){
+			checkBox2.setDisable(true);
 		}
-		if (!male.isSelected()){
-			female.setDisable(false);
+		if (!checkBox1.isSelected()){
+			checkBox2.setDisable(false);
 		}
-		if(female.isSelected()){
-			male.setDisable(true);
+		if(checkBox2.isSelected()){
+			checkBox1.setDisable(true);
 		}
-		if (!female.isSelected()){
-			male.setDisable(false);
+		if (!checkBox2.isSelected()){
+			checkBox1.setDisable(false);
 		}
 		
 	}
