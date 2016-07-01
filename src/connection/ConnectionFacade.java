3429f5827af8c7623669e0e23aa41a2c9a8e358e
davydov-denis <den.73.rus@mail.ru>
2016-07-02 01:00:11 +0400

    implemeted from Querable<Things>

--- src/connection/ConnectionFacade.java
+++ src/connection/ConnectionFacade.java
@@ -1,9 +1,14 @@
 package connection;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
-public class ConnectionFacade {
+import entities.Things;
+import javafx.collections.ObservableList;
+
+public class ConnectionFacade implements Querable<Things>{
 
 	private Statement stmnt;
 	
@@ -11,4 +16,18 @@ public class ConnectionFacade {
 		if (stmnt == null || stmnt.isClosed())
 			stmnt = SQLConnection.connect();
 	}
+
+	@Override
+	public List<Things> query(String arg) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ObservableList<Things> sortedObservableQuery(String arg) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 }
