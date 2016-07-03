773e39c7a3f92b18250a21437257401531324f30
davydov-denis <den.73.rus@mail.ru>
2016-07-03 23:33:33 +0400

    rewritten test for db

--- src/test/DBTest.java
+++ src/test/DBTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import connection.ConnectionFacade;
@@ -12,9 +11,7 @@ public class DBTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Params p = new Params().setRange("0-10").setTipeTp("Ћес").setCountD("10").setCountPR("3").setPrecipitation("ясно");
-		List<Things> list = new ArrayList<Things>();
-		
-		ConnectionFacade.someMethod(list, "Things", p);
+		List<Things> list =  ConnectionFacade.query(p);
 		list.forEach((thing) -> System.out.println(thing));
 	}
 
