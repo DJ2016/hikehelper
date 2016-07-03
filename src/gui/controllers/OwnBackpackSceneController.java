930af1cbda4e355484a4221afbf5097fd3963df3
davydov-denis <den.73.rus@mail.ru>
2016-07-03 18:44:35 +0400

    added listener for focusedproperty of textfields

--- src/gui/controllers/OwnBackpackSceneController.java
+++ src/gui/controllers/OwnBackpackSceneController.java
@@ -1,13 +1,21 @@
 package gui.controllers;
 
+
+import javafx.event.EventHandler;
+
 import java.util.Arrays;
 
 import entities.Things;
+import gui.listeners.AcceptChangeListener;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import util.Util;
+import javafx.scene.effect.*;
+import javafx.scene.text.Text;
 
 public class OwnBackpackSceneController extends AbstractController {
 
@@ -23,15 +31,18 @@ public class OwnBackpackSceneController extends AbstractController {
 	@FXML @Override
 	public void initialize() {
 		tableThings.setItems(things);
+		quanInput.focusedProperty().addListener(new AcceptChangeListener<Node, Boolean>(quanInput, this::setNodeStyle, false));
+		nameInput.focusedProperty().addListener(new AcceptChangeListener<Node, Boolean>(nameInput, this::setNodeStyle, false));
 	}
 
+	
 	@FXML
 	private void addClickedButton() {
 		if (nameInput.getText().isEmpty()) {
 			new AutoShowableAlert("Ошибка", "Заполните поле Предмет.");
 		} else {
 			things.add(new Things().safeSet(nameInput.getText(), quanInput.getText()));
-			Util.invokeAll(Arrays.asList(nameInput, quanInput), TextField.class, "clear");
+			//Util.invokeAll(Arrays.asList(nameInput, quanInput), TextField.class, "clear");
 		}
 	}
 
