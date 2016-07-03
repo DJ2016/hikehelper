529f1d9c70d5fa45976a45810a5e68fabc01018b
davydov-denis <den.73.rus@mail.ru>
2016-07-03 22:16:20 +0400

    created params

---
+++ src/entities/Params.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Params {
+	private String range;
+	private String precipitation;
+	private String tipeTp;
+	private String countPR;
+	private String countD;
+	
+	public Params(){}
+	
+	public String getTipeTp() {
+		return tipeTp;
+	}
+	public Params setTipeTp(String tipeTp) {
+		this.tipeTp = tipeTp;
+		return this;
+	}
+	
+	public String getCountD() {
+		return countD;
+	}
+	public Params setCountD(String countD) {
+		this.countD = countD;
+		return this;
+	}
+	public String getCountPR() {
+		return countPR;
+	}
+	public Params setCountPR(String countPR) {
+		this.countPR = countPR;
+		return this;
+	}
+	public String getPrecipitation() {
+		return precipitation;
+	}
+	public Params setPrecipitation(String precipitation) {
+		this.precipitation = precipitation;
+		return this;
+	}
+	public String getRange() {
+		return range;
+	}
+	public Params setRange(String range) {
+		this.range = range;
+		return this;
+	}
+}
