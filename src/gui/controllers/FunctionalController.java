790c72222f9fa47477366c68adccdfbd2e8b82ac
davydov-denis <den.73.rus@mail.ru>
2016-07-03 18:39:23 +0400

    moved AutoShowableAlert from AbstractController, added method and constructor

--- src/gui/controllers/FunctionalController.java
+++ src/gui/controllers/FunctionalController.java
@@ -3,13 +3,20 @@ package gui.controllers;
 import java.io.IOException;
 import java.lang.FunctionalInterface;
 import java.util.function.Function;
+
+import javafx.scene.control.Alert;
 import javafx.scene.control.Tooltip;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import fxmls.FXMLFrameLoader;
 import gui.App;
 import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
+import javafx.util.Pair;
 
 @FunctionalInterface
 public interface FunctionalController<T extends Node> extends Function<FunctionalController<T>, Void>{
@@ -61,5 +68,51 @@ public interface FunctionalController<T extends Node> extends Function<Functiona
 	 */
 	public default void hideTooltip(){
 		tip.hide();
+	}
+	
+	
+
+	/**
+	 * there are an implementation of {@link javafx.scene.control.Alert}
+	 * with auto showing
+	 */
+	public class AutoShowableAlert extends Alert{
+		/**
+		 * @param title the Title of the dialog
+		 * @param information the string to show in the dialog header area.
+		 */
+		public AutoShowableAlert(String title, String information){
+			this(title, information, Alert.AlertType.ERROR);
+		}
+		/**
+		 * Creates an alert with the given AlertType 
+		 * @param title the Title of the dialog
+		 * @param information the string to show in the dialog header area.
+		 * @param type 
+		 */
+		public AutoShowableAlert(String title, String information, AlertType type){
+			super(type);
+			super.setTitle(title);
+			super.setHeaderText(information);
+			super.show();
+		}
+		
+		/**
+		 * returns an alert with the given args
+		 * @param lookup this arg provides a way in which developers 
+		 * 		  may retrieve the actual Node for a given and set text on it
+		 * @param context of alert
+		 * @param header of alert
+		 * @return instance
+		 */
+		public static Alert lookupInstance(Pair<ButtonType, String> lookup, String context, String header){
+			Alert alert = new Alert(Alert.AlertType.CONFIRMATION, context);
+			if (lookup != null)
+				((Button) alert.getDialogPane().lookupButton(lookup.getKey())).setText(lookup.getValue());
+			alert.setHeaderText(header);
+			alert.initModality(Modality.APPLICATION_MODAL);
+			return alert;
+		}
+
 	}
 }
