efbd5b507ec620ead47f7630f051b626f0475d82
davydov-denis <den.73.rus@mail.ru>
2016-07-03 18:40:02 +0400

    refactored confirmation on exit

--- src/gui/App.java
+++ src/gui/App.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 import fxmls.FXMLFrameLoader;
+import gui.controllers.FunctionalController.AutoShowableAlert;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
@@ -14,67 +15,56 @@ import javafx.scene.control.ButtonType;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.util.Pair;
 
 public class App extends Application {
 
 	protected static Scene scene;
 	protected static Stage stage;
 
-	public static Scene getScene(){
+	public static Scene getScene() {
 		return scene;
 	}
 
-	public static Stage getStage(){
+	public static Stage getStage() {
 		return stage;
 	}
-
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-
 
 	@Override
 	public void start(Stage stage) throws Exception {
 		App.stage = stage;
 		setFrame(FXMLFrameLoader.getMainFrame(), "Собери Рюкзак в поход!");
 		stage.setOnCloseRequest(confirmCloseEventHandler);
-		
+
 	}
-	
+
 	private EventHandler<WindowEvent> confirmCloseEventHandler = event -> {
-        Alert closeConfirmation = new Alert(
-                Alert.AlertType.CONFIRMATION,
-                "Вы уверены, что хотите выйти?"
-        );
-        Button exitButton = (Button) closeConfirmation.getDialogPane().lookupButton(
-                ButtonType.OK
-        );
-        exitButton.setText("Выход");
-        closeConfirmation.setHeaderText("Подтвердите выход");
-        closeConfirmation.initModality(Modality.APPLICATION_MODAL);
+		Alert closeConfirmation = AutoShowableAlert
+				.lookupInstance(new Pair<>(ButtonType.OK, "Выход"), "Вы уверены, что хотите выйти?", "Подтвердите выход");
+		Optional<ButtonType> closeResponse = closeConfirmation.showAndWait();
+		if (!ButtonType.OK.equals(closeResponse.get())) {
+			event.consume();
+		}
+	};
 
-        Optional<ButtonType> closeResponse = closeConfirmation.showAndWait();
-        if (!ButtonType.OK.equals(closeResponse.get())) {
-            event.consume();
-        }
-    };
-
-
-	public static void setFrame(Parent root) throws IOException{
+	public static void setFrame(Parent root) throws IOException {
 		setFrame(root, "");
 	}
 
-	public static void setFrame(String frameName, String title) throws IOException{
+	public static void setFrame(String frameName, String title) throws IOException {
 		setFrame(FXMLFrameLoader.getRoot(frameName), title);
 
 	}
 
-	public static void setFrame(String frameName) throws IOException{
+	public static void setFrame(String frameName) throws IOException {
 		setFrame(frameName, "");
 	}
 
-	public static void setFrame(Parent root, String title) throws IOException{
+	public static void setFrame(Parent root, String title) throws IOException {
 		scene = new Scene(root);
 
 		if (stage.isShowing())
