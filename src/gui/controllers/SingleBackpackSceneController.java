020644bfea712d7c65edd668465644dfe8bb953b
davydov-denis <den.73.rus@mail.ru>
2016-07-04 15:12:31 +0400

    connected frame and request and database

--- src/gui/controllers/SingleBackpackSceneController.java
+++ src/gui/controllers/SingleBackpackSceneController.java
@@ -1,14 +1,43 @@
 package gui.controllers;
 
+import java.sql.SQLException;
+
+import connection.ConnectionFacade;
+import entities.Params;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 public class SingleBackpackSceneController extends AbstractController{
 
 	@FXML private CheckBox checkBox1;
 	@FXML private CheckBox checkBox2;
 
+	@FXML
+	private ChoiceBox<String> boxPrecipitation;
+	
+	@FXML
+	private ChoiceBox<String> boxTipeTp;
+	
+	@FXML
+	private ChoiceBox<String> boxCountDays;
+	
+	@FXML
+	private TextField fieldFrom;
+	
+	@FXML
+	private TextField fieldTo;
+	
+	@FXML
+	private TextField fieldWeight;
+	
+	@FXML
+	private TextField fieldAge;
+	
+	
+	
 	@FXML
 	public void isChecked(){
 		checkBox1.setDisable(checkBox2.isSelected());
@@ -22,7 +51,17 @@ public class SingleBackpackSceneController extends AbstractController{
 		// TODO Auto-generated method stub
 	}
 
-
+	@FXML
+	public void onCreatedClicked() throws ClassNotFoundException, SQLException{
+		Params params = new Params()
+				.setPrecipitation(boxPrecipitation.getValue())
+				.setCountD(boxCountDays.getValue())
+				.setCountPR("1")
+				.setRange(fieldFrom.getText() + "-" + fieldTo.getText())
+				.setTipeTp(boxTipeTp.getValue());
+		tableThings.setItems(ConnectionFacade.sortedObservableQuery(params));
+	}
+	
 	@Override
 	public Void apply(FunctionalController<Node> t) {
 		// TODO Auto-generated method stub
