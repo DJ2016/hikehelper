b90749826eab15871017b5a20ed21fc84b318716
davydov-denis <den.73.rus@mail.ru>
2016-07-06 00:19:08 +0400

    simple flip img

--- src/gui/controllers/Controller.java
+++ src/gui/controllers/Controller.java
@@ -2,18 +2,31 @@ package gui.controllers;
 
 import java.io.IOException;
 
+import javafx.application.Application;
 import entities.Product;
 import fxmls.FXMLFrameLoader;
 import gui.App;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TableView;
 import parser.SportmasterParser;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogEvent;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.stage.PopupWindow;
+import javafx.event.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.Group;
 
 public class Controller implements FunctionalController<TextField>{
 	
@@ -70,14 +83,43 @@ public class Controller implements FunctionalController<TextField>{
 
 			@Override
 			public void changed(ObservableValue<? extends Product> observable, Product oldValue, Product newValue) {
-				/*
-				Dialog<?> d = new Dialog<>();
-				d.getDialogPane().setContent(new ImageView(observable.getValue().getImgSource()));
-				d.showAndWait();
-				*/
+				
+				Button left = new Button("Влево");
+				Button right = new Button("Вправо");
+				ImageView imgview = new ImageView(observable.getValue().getImgSource());
+				
+				imgview.toBack();
+				left.toFront();
+				right.toFront();
+				
+				Stage stage = new Stage();
+				Group group = new Group();
+				
+				StackPane imgPane = new StackPane(imgview);
+				imgPane.setAlignment(Pos.CENTER);
+				
+				StackPane btnLeftPane = new StackPane(left);
+				imgPane.setAlignment(Pos.CENTER);
+				
+				StackPane btnRightPane = new StackPane(right);
+				imgPane.setAlignment(Pos.CENTER);
+				
+				group.getChildren().add(imgPane);
+				
+				Scene sc = new Scene(group);
+				
+				stage.setScene(sc);
+				stage.show();
+		
+				group.getChildren().add(btnLeftPane);
+				group.getChildren().add(btnRightPane);
+				btnRightPane.setLayoutX(118);
+				btnLeftPane.setLayoutX(2);
+				System.out.println(observable.getValue().getImgSource());
 			}
 		});
 	}
+
 
 
 	@FXML
